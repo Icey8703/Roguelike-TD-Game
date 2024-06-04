acb2048979f2aa9b24f557776ad30e18d03b4a92
Icey8703 <kieranbayne@icloud.com>
2024-06-04 09:55:56 -0700

    ALmost done
    added exit button to main menu, menu button to gameplay scene, and other things, finish the GOD DAMN MENU VISUALS

---
+++ Assets/Scripts/ExitButtonScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Quits the app when the exit button is clicked sunglasses emoji
+public class ExitButtonScript : MonoBehaviour
+{
+    
+    public void OnClicked()
+    {
+
+        Application.Quit();
+
+    }
+
+}
